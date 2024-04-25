6bcdc6c25ef2b7ce8e0710ed792e624619356bfd
trevorjtclarke <trevorjtclarke@gmail.com>
2024-04-25 11:46:09 -0700

    remove changes to example

--- example/next/pages/index.tsx
+++ example/next/pages/index.tsx
@@ -1,10 +1,9 @@
 import { Center, HStack, Spacer, Stack, Text, useColorMode } from "@chakra-ui/react";
-import { useAccount, getRecentChainIds, useCapsule } from "graz";
+import { useAccount, useCapsule } from "graz";
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
 import { BalanceList } from "ui/balance-list";
 import { ChainSwitcher } from "ui/chain-switcher";
-import { ChainSigner } from "ui/chain-signer";
 import { ConnectButton } from "ui/connect-button";
 import { ConnectStatus } from "ui/connect-status";
 import { ToggleTheme } from "ui/toggle-theme";
@@ -15,16 +14,11 @@ const LeapSocialLogin = dynamic(
 );
 
 const HomePage: NextPage = () => {
-  const recentChainIds = getRecentChainIds() || [];
-  const latestChainId = recentChainIds[recentChainIds.length - 1] || 'cosmoshub-4';
-  
   const { data: accountData } = useAccount({
-    chainId: latestChainId,
-    multiChain: false,
+    chainId: "cosmoshub-4",
   });
   const { client, modalState, onAfterLoginSuccessful, setModalState, onLoginFailure } = useCapsule();
   const { colorMode } = useColorMode();
-
   return (
     <Center minH="100vh">
       <Stack bgColor="whiteAlpha.100" boxShadow="md" maxW="md" p={4} rounded="md" spacing={4} w="full">
@@ -39,8 +33,6 @@ const HomePage: NextPage = () => {
             <Text noOfLines={1} wordBreak="break-all">
               Wallet address: <b>{accountData.bech32Address}</b>
             </Text>
-
-            {latestChainId && <ChainSigner />}
 
             <BalanceList />
             <ChainSwitcher />
--- example/next/utils/graz.ts
+++ example/next/utils/graz.ts
@@ -1,7 +1,6 @@
-import { cosmoshub, osmosistestnet, stargaze } from "graz/chains";
+import { cosmoshub, osmosistestnet } from "graz/chains";
 
 export const chains = [
   { ...cosmoshub, rpc: "https://rpc.cosmos.directory/cosmoshub", rest: "https://rest.cosmos.directory/cosmoshub" },
   osmosistestnet,
-  stargaze,
 ];
